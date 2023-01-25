c87804d77b17c12af871f2cee0d795ce7ed9a884
Tai (he/him) <116750472+tai-de@users.noreply.github.com>
2023-01-25 16:08:12 -0700

    feature: admin-dashboard, update controller to query DB counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["BASIC_AUTH_USER"], password: ENV["BASIC_AUTH_PASS"]
 
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
 end
