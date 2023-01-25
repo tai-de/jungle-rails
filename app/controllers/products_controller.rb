281853415948d8215428925b527fcc51a75f413c
Tai (he/him) <116750472+tai-de@users.noreply.github.com>
2023-01-25 08:38:42 -0700

    Add dynamic subtitle

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,9 @@
 class ProductsController < ApplicationController
 
   def index
+    messages = ["WHERE YOU CAN FIND ANY PLANTS!", "IT GETS WORSE HERE EVERY DAY", "WE GOT EVERYTHING YOU WANT"]
+    
+    @message = messages.sample
     @products = Product.all.order(created_at: :desc)
   end
 
