0165ef9c09cc5a61889401b5543c2d46615d08c2
Tai (he/him) <116750472+tai-de@users.noreply.github.com>
2023-01-26 08:17:46 -0700

    fix: update routes to include categories resources under admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-    get 'categories/index'
-    get 'categories/new'
-    get 'categories/create'
-  end
   get '/about', to: "about#index"
   root to: 'products#index'
 
@@ -21,6 +16,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
