38663a402d8fef3345de19291a2fddaad0c1788a
Tai (he/him) <116750472+tai-de@users.noreply.github.com>
2023-01-26 09:39:04 -0700

    update sessions controller & application with helper method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    
+    # If the user exists AND password is correct
+    if user && user.authenticate(params[:password])
+      # Save user id in browser cache
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/login'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
   end
 end
