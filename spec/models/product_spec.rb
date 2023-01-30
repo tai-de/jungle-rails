163b6da6d09dfb4f24c7ba8628ada0dca63b3664
Tai (he/him) <116750472+tai-de@users.noreply.github.com>
2023-01-30 15:02:37 -0700

    refactor Product spec tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,42 +2,39 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe "Validations" do
-    
-    it "saves successfully when all fields are valid" do
+
+    before(:each) do
       @category = Category.create!(name: 'Foliage')
       @product = @category.products.new(name: 'Plant', price_cents: 10000, quantity: 10)
-      expect(@product.save!).to be true
-      # @product_found = Product.find_by(name: 'Plant')
-      # pp @product_found
+    end
+    
+    it "saves successfully when all fields are valid" do
+      expect(@product.save!).to be true\
     end
 
     it "fails when name is empty" do
-      @category = Category.create!(name: 'Foliage')
-      @product = @category.products.new(name: nil, price_cents: 10000, quantity: 10)
+      @product.name = nil
       @product.save
-      expect(@product.errors.full_messages).to include("Name can't be blank")
+      expect(@product.errors[:name]).to include("can't be blank")
     end
 
     it "fails when price is empty" do
-      @category = Category.create!(name: 'Foliage')
-      @product = @category.products.new(name: 'Plant', price_cents: nil, quantity: 10)
+      @product.price_cents = nil
       @product.save
-      expect(@product.errors.full_messages).to include("Price can't be blank")
+      expect(@product.errors[:price]).to include("can't be blank")
       
     end
 
     it "fails when quantity is empty" do
-      @category = Category.create!(name: 'Foliage')
-      @product = @category.products.new(name: 'Plant', price_cents: 10000, quantity: nil)
+      @product.quantity = nil
       @product.save
-      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+      expect(@product.errors[:quantity]).to include("can't be blank")
     end
 
     it "fails when category is empty" do
-      @category = Category.create!(name: 'Foliage')
-      @product = Product.new(name: 'Plant', price_cents: 10000, quantity: 10)
+      @product.category = nil
       @product.save
-      expect(@product.errors.full_messages).to include("Category can't be blank")
+      expect(@product.errors[:category]).to include("can't be blank")
     end
   end
 end
