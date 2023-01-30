ae1f1b7322291bd1b3720ed90278678c1f3b5b66
Tai (he/him) <116750472+tai-de@users.noreply.github.com>
2023-01-30 15:05:10 -0700

    refactor User spec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,35 +3,37 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "Validations" do
 
-    it "saves successfully when all fields are valid" do
+    before(:each) do
       @user = User.new(first_name: "Tai", last_name: "Delisle", email: "tai@tai.tai", password: "password", password_confirmation: "password")
+    end
+
+    it "saves successfully when all fields are valid" do
       @user.save
       expect(@user).to be_valid
     end
 
     it "saves successfully when password and password_confirmation match" do
-      @user = User.new(first_name: "Tai", last_name: "Delisle", email: "tai@tai.tai", password: "password", password_confirmation: "password")
       @user.save
       expect(@user).to be_valid
     end
 
     it "fails when password and password_confirmation do not match" do
-      @user = User.new(first_name: "Tai", last_name: "Delisle", email: "tai@tai.tai", password: "password", password_confirmation: "doesntmatch")
+      @user.password_confirmation = "notmatching"
       @user.save
 
       expect(@user).not_to be_valid
     end
 
     it "fails when password is not given" do
-      @user = User.new(first_name: "Tai", last_name: "Delisle", email: "tai@tai.tai")
+      @user.password = nil
+      @user.password_confirmation = nil
       @user.save
 
       expect(@user).not_to be_valid
     end
 
     it "must prevent duplicate email addresses" do
-      @user1 = User.new(first_name: "Tai", last_name: "Delisle", email: "tai@tai.tai", password: "password", password_confirmation: "password")
-      @user1.save
+      @user.save
 
       @user2 = User.new(first_name: "AnotherTai", last_name: "ForReal", email: "tai@tai.TAI", password: "password", password_confirmation: "password")
       @user2.save
@@ -46,7 +48,8 @@ RSpec.describe User, type: :model do
     end
 
     it "must fail if password does not meet minimum length" do
-      @user = User.new(first_name: "Tai", last_name: "Delisle", email: "tai@tai.tai", password: "p", password_confirmation: "p")
+      @user.password = "p"
+      @user.password_confirmation = "p"
       @user.save
 
       expect(@user).not_to be_valid
@@ -56,31 +59,24 @@ RSpec.describe User, type: :model do
 
   describe ".authenticate_with_credentials" do
 
-    it "should log in with valid credentials" do
+    before(:each) do
       @user = User.new(first_name: "Tai", last_name: "Delisle", email: "tai@tai.tai", password: "password", password_confirmation: "password")
       @user.save
+    end
 
+    it "should log in with valid credentials" do
       expect(User.authenticate_with_credentials("tai@tai.tai", "password")).to be_truthy
     end
 
     it "should not log in with invalid credentials" do
-      @user = User.new(first_name: "Tai", last_name: "Delisle", email: "tai@tai.tai", password: "password", password_confirmation: "password")
-      @user.save
-
       expect(User.authenticate_with_credentials("tai@tai.tai", "wrongpassword")).to be_falsy
     end
 
     it "should log in when emails are case insensitive" do
-      @user = User.new(first_name: "Tai", last_name: "Delisle", email: "tai@tai.tai", password: "password", password_confirmation: "password")
-      @user.save
-
       expect(User.authenticate_with_credentials("tai@TAI.tai", "password")).to be_truthy
     end
 
     it "should log in when email has extra spaces" do
-      @user = User.new(first_name: "Tai", last_name: "Delisle", email: "tai@tai.tai", password: "password", password_confirmation: "password")
-      @user.save
-
       expect(User.authenticate_with_credentials(" tai@TAI.tai  ", "password")).to be_truthy
     end
 
